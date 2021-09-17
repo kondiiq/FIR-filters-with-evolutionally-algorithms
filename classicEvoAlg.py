70ca3f85a98c0ad36eb45c59f38af0d7b26e104f
Kowalczyk <konrad.kowalczyk@capgemini.com>
2021-09-17 15:36:56 +0200

    Added description of function and import packages plus writing definition with empty body of functions

--- classicEvoAlg.py
+++ classicEvoAlg.py
@@ -1,31 +1,59 @@
 import random
-
-'''
-Funckja generujca pojedyncza probke
-
-Function which generating single sample
-'''
+import pandas
+#import matplotlib
+#import Scipy
+import numpy
 
 
 def create_sample():
+    """
+    Funkcja generująca pojedynczą próbkę
+
+    Function which generating single sample
+    ":return float sample
+    """
     sample = random.random()
     return sample
 
 
-'''
-Funkcja generujaca zadana populacje wykorzystujaca funkcje create_sample
-
-Function which generating population using create_sample()
-'''
-
-
 def create_starting_population(number_of_sample: int = 200):
+    """
+    Funkcja generująca zadaną populację wykorzystującą funkcje create_sample
+
+    Function which generating population using create_sample()
+    :parameter number_of_sample
+    :return array of samples
+    """
     samples = []
-    for number in range(0, number_of_sample):
-        create_sample()
-        samples.append(create_sample())
-    print(samples)
+
+    if number_of_sample <= 0:
+        raise ValueError('Array should have more than 0 samples and must not have negative numbers of index!!')
+
+    else:
+        for number in range(0, number_of_sample):
+            create_sample()
+            samples.append(create_sample())
+    #print(samples)
     return samples
+
+
+def choose_best_samples():  # potrzebuje listy !!!
+    """
+    Funkcja sortująca wszystkie próbki oraz wyszukująca najlepsze próbki z przekazanej tablicy próbek
+    Function which sort samples and finding best samples from common array
+    :parameter array, number of needed samples
+    :return: best n samples of array
+    """
+    pass
+
+
+def cross_samples():
+    """
+    Funckja wyszukująca najlepsze według algorytmu próbki i dodające je do nowej listy
+    Function which find
+    :return array of samples
+    """
+    choose_best_samples()
 
 
 if __name__ == "__main__":
