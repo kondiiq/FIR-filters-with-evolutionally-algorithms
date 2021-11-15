9f441f94edb18df0667ca461b8ac6c64b0ce99c4
Konrad <kowalczykkon@protonmail.com>
2021-11-15 22:06:53 +0100

    Implemented wk_var function and starting working with PSO algorithm

--- PSO_alg.py
+++ PSO_alg.py
@@ -1,56 +1,57 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import scipy as sc
-import geneal as gea
 
 
-def transfer_func(size:int, population):
+def transfer_func(size: int, population):
     vz = 0
 
     for m in range(0, population):
         vz += population[m] * 1 ** (-1)
     pass
 
+
 def initialize_single_solution(size: int):
-    '''
+    """
     :param size: Represent order of designed filter
     :return: Random pseudo-numbers list of solution
-    '''
+    """
 
     return np.random.random_sample(size)
 
 
-def omega_n(coeff_list, M):
-    omegaN = []
-
+def omega_n(coeff_list, m):
     for n in coeff_list:
-        omegaN.append((2 * np.pi * n) / (M + 1))
+        omegan.append((2 * np.pi * n) / (m + 1))
 
-    return omegaN
+    return omegan
 
-def vvomega(M, omega_n, coeff_list):
+
+def vvomega(order, omega_n, coeff_list):
     vomega = []
 
-    for m in range(0, M):
-            element = ( coeff_list[m] * np.exp(- 1j * omega_n[m] * m))
-            vomega.append(element)
+    for m in range(0, order):
+        element = (coeff_list[m] * np.exp(- 1j * omega_n[m] * m))
+        vomega.append(element)
 
     return vomega
 
 
 def fitness_func(size: int, vomega, band_pass, band_stop):
     lst = []
-    f1 = 0
-    f2 = 0
 
     for iterate in range(0, size):
-        f1 = ( np.abs( np.abs ( vomega[iterate]) -1) - band_pass) ** 2
-        f2 = ( np.abs( np.abs( vomega[iterate])-1) - band_stop) ** 2
+        f1 = (np.abs(np.abs(vomega[iterate]) - 1) - band_pass) ** 2
+        f2 = (np.abs(np.abs(vomega[iterate]) - 1) - band_stop) ** 2
         fsum = f1 + f2
         lst.append(fsum)
-
     return lst
+
+
+def wk(wmax: float, wmin: float, k: int, itu: int):  # rownanie 10
+    return wmax - (k / itu) * (wmax - wmin)
+
+
+def generate_r_1_r_2(min_val: float, max_val: float):
+    return np.random.uniform(low=min_val, high=max_val, size=1)
 
 
 if __name__ == "__main__":
@@ -58,21 +59,27 @@ if __name__ == "__main__":
     population = []
     M = 40
     numbers_of_iterations = 400
+    min_r = 0.1
+    max_r = 6.17
+    alpha_1 = 1.9
+    alpha_2 = 1.8
     for i in range(0, number_of_solution):
         genotype = initialize_single_solution(M)
-    # print(population)
+        # print(population)
         omegan = omega_n(genotype, M)
         vomeg = vvomega(M, omegan, genotype)
-        this_solution = fitness_func(M, vomeg, 0.6554, 0.7257) # wartosci zabrane z Dystrybuanta Φ(x) standardowego rozkładu normalnego N (0, 1) dla 0.4 (dwa pasma 0.2) i 0.6
+        this_solution = fitness_func(M, vomeg, 0.6554, 0.7257)  # wartosci zabrane z Dystrybuanta Φ(x) standardowego
+        # rozkładu normalnego N (0, 1) dla 0.4 (dwa pasma 0.2) i 0.6
         population.append(this_solution)
-
 
     for i in range(0, number_of_solution):
         print(f'Population #number{i} {population[i]}\n')
 
-###     START PSO ALGORITHM
+    ###     START PSO ALGORITHM
 
     for iteration in range(0, numbers_of_iterations):
         for genes in range(0, M):
-            pass
+            wk(0.9, 0.4, iteration, numbers_of_iterations)
+            # eq8 uk_plus_one
+            # eq9 pk_plus_one
         pass
